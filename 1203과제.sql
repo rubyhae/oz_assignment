077f100e305eac4e0afedc95628709f40f4dcc36
Ibiella <rubyhae@gmail.com>
2024-12-03 14:48:12 +0900

    ✨ 1203과제추가
    과제파일 선 업로드

---
+++ 1203과제.sql
@@ -0,0 +1,14 @@
+insert into customers (customenumber, customername, ~~~) value (341, 'jane');
+-- customers 테이블에 새 고객을 추가하세요
+insert into products (productcode, productname, ~~) value (1771, 'lego', ~~);
+-- products 테이블에 새 제품을 추가하세요
+insert into employees (enployeenumber, lastname, firstname, ~~) value (123782, 'john', 'doe');
+-- employees 테이블에 새 직원을 추가하세요
+insert into offices (officename, address, contact ~~) value ('서초오피스', '서울특별시 서초구 ~~', '02-0000-0000');
+-- offices 테이블에 새 사무실을 추가하세요.
+insert into orders (orderdate, customerid, amount, total price) value ('2024. 12.03', 'john_doe', 12, 43.32);
+-- orders 테이블에 새 주문을 추가하세요
+insert into orderdetails (ordernumber, productcode, ~~) value (12389713, 33232, ~~);
+-- orderdetails 테이블에 주문 상세 정보를 추가하세요
+insert into payments (customernumber, checknumber, ~~) value (87667, 898982, ~~);
+-- payments 테이블에 지불 정보를 추가하세요
