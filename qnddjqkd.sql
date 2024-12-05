9fe4c9ea174b05b52874c7524cc921685c229286
Ibiella <rubyhae@gmail.com>
2024-12-05 22:57:38 +0900

    modify

--- qnddjqkd.sql
+++ qnddjqkd.sql
@@ -15,34 +15,3 @@ connection = pymysql.connect(
     cursorclass=DictCursor,
 )
 
-try:
-    with connection.cursor() as cursor:
-        for _ in range(10):
-            first_name = fake.first_name()
-            last_name = fake.last_name()
-            email = fake.unique.email()
-            password = fake.password()
-            address = fake.address()
-            contact = fake.phone_number()
-            gender = random.choice(["MALE", "FEMALE"])
-
-            cursor.execute(
-                """
-                INSERT INTO users (first_name, last_name, email, password, address, contact, gender)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-            """,
-                (first_name, last_name, email, password, address, contact, gender),
-            )
-
-        for _ in range(5):
-            name = fake.country()
-            address = fake.address()
-            contact = fake.phone_number()
-
-            cursor.execute(
-                """
-                INSERT INTO stores (name, address, contact)
-                VALUES (%s, %s, %s)
-            """,
-                (name, address, contact),
-            )
