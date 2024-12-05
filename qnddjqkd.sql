3e7c212ec1a02b040c86d38778e4961979198952
Ibiella <rubyhae@gmail.com>
2024-12-05 21:48:07 +0900

    🎨 1205 수정
    붕어빵 sql 수정 진행 랜덤 데이터 입력 가능하게 변경함

--- qnddjqkd.sql
+++ qnddjqkd.sql
@@ -1,14 +1,48 @@
-INSERT INTO users (first_name, last_name, email, password, gender) VALUE
-	('jane', 'doe', 'jane@test.com', '123232', 'Female'),
-    ('john', 'doe', 'john@test.com', '338282', 'Male'),
-    ('철수', '김', 'cheol@test.com', '21831987', 'Male'),
-    ('영희', '최', 'choi.yh@test.com', '123132', 'Female'),
-    ('유리', '박', 'p.yuri@test.com', '1231123', 'Female'),
-    ('지운', '정', 'jjw@test.com', 'u898989', 'Female'),
-    ('하나', '윤', 'yhana@test.com', 'dd89898', 'Female'),
-    ('정훈', '신', 'jhs@test.com', 'ddfdwddw', 'Male'),
-    ('온누리', '김', 'konr@test.com', '12312dsd', 'Male'),
-    ('유나', '성', 'sungyuna@test.com', 'dd989dd', 'Female');
-    
+import random
 
-    
+import pymysql
+from faker import Faker
+from pymysql.cursors import DictCursor
+
+fake = faker
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",  # 사용자 이름
+    password="0000",  # 비밀번호
+    database="db_name", # db 이름
+    charset="utf8mb4",
+    cursorclass=DictCursor,
+)
+
+try:
+    with connection.cursor() as cursor:
+        for _ in range(10):
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            email = fake.unique.email()
+            password = fake.password()
+            address = fake.address()
+            contact = fake.phone_number()
+            gender = random.choice(["MALE", "FEMALE"])
+
+            cursor.execute(
+                """
+                INSERT INTO users (first_name, last_name, email, password, address, contact, gender)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """,
+                (first_name, last_name, email, password, address, contact, gender),
+            )
+
+        for _ in range(5):
+            name = fake.country()
+            address = fake.address()
+            contact = fake.phone_number()
+
+            cursor.execute(
+                """
+                INSERT INTO stores (name, address, contact)
+                VALUES (%s, %s, %s)
+            """,
+                (name, address, contact),
+            )
